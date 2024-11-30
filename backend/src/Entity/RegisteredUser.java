669c5e5bc38b7a068ffd893bb0a7a9389f85da55
DavidNg32 <dangducduy.nguyen@ucalgary.ca>
2024-11-30 13:52:15 -0700

    Filled in some classes LMAO

--- backend/src/Entity/RegisteredUser.java
+++ backend/src/Entity/RegisteredUser.java
@@ -1,4 +1,70 @@
 package Entity;
 
-public class RegisteredUser {
+import java.util.Date;
+
+public class RegisteredUser extends User{
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private UserBankingInfo paymentInfo;
+    private Date dateToPayFee;
+    
+    public RegisteredUser(String userID, boolean userType, TicketCart cart, String userEmail, String username, 
+                          String password, String firstName, String lastName, String address, UserBankingInfo paymentInfo
+            , Date dateToPayFee) {
+        super(userID, userType, cart, userEmail);
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.paymentInfo = paymentInfo;
+        this.dateToPayFee = dateToPayFee;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public UserBankingInfo getPaymentInfo() {
+        return paymentInfo;
+    }
+    public void setPaymentInfo(UserBankingInfo paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+    public Date getDateToPayFee() {
+        return dateToPayFee;
+    }
+    public void setDateToPayFee(Date dateToPayFee) {
+        this.dateToPayFee = dateToPayFee;
+    }
+    
 }
--- backend/src/Entity/User.java
+++ backend/src/Entity/User.java
@@ -1,4 +1,40 @@
 package Entity;
 
 public class User {
+    private String userID;
+    private boolean userType;
+    private TicketCart cart;
+    private String userEmail;
+    
+    public User(String userID, boolean userType, TicketCart cart, String userEmail) {
+        this.userID = userID;
+        this.userType = userType;
+        this.cart = cart;
+        this.userEmail = userEmail;
+    }
+    public String getUserID() {
+        return userID;
+    }
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    public boolean isUserType() {
+        return userType;
+    }
+    public void setUserType(boolean userType) {
+        this.userType = userType;
+    }
+    public TicketCart getCart() {
+        return cart;
+    }
+    public void setCart(TicketCart cart) {
+        this.cart = cart;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    
 }
