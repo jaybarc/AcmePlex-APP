970913219200a31dbafe493871c0625a3689b887
DavidNg32 <dangducduy.nguyen@ucalgary.ca>
2024-11-30 15:15:43 -0700

    We cannot be more sensitive to pleasure without being more sensitive to pain - Alan Watts

--- backend/src/Entity/ScreeningRoom.java
+++ backend/src/Entity/ScreeningRoom.java
@@ -1,4 +1,17 @@
 package Entity;
 
+import java.util.ArrayList;
+
 public class ScreeningRoom {
+    private Theater theater;
+    private String roomID;
+    private ArrayList<Seat> available_seat;
+    private int number_of_seats;
+    
+    public ScreeningRoom(Theater theater, String roomID, ArrayList<Seat> available_seat, int number_of_seats) {
+        this.theater = theater;
+        this.roomID = roomID;
+        this.available_seat = available_seat;
+        this.number_of_seats = number_of_seats;
+    }
 }
