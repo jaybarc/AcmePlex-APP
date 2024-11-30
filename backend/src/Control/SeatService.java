070636d00186e7f0fe5885337f4a786ed075d4b9
jaybarc <jerome.barcelona@gmail.com>
2024-11-30 15:03:35 -0700

    made moves for database

--- backend/src/Control/SeatService.java
+++ backend/src/Control/SeatService.java
@@ -1,15 +1,15 @@
-package Entity;
+package Control;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import Entity.Seat;
 
 public class SeatService {
 
-    // Database connection settings
     private static final String URL = "jdbc:mysql://localhost:3306/acmeplexdb";
-    private static final String USER = "your_username";
-    private static final String PASSWORD = "your_password";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Jeromebarcelona01!";
 
     // Get all available seats
     public List<Seat> getAvailableSeats() {
