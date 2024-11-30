1c4bf37abc2491de1843b37e49d845b0f4e79053
jaybarc <jerome.barcelona@gmail.com>
2024-11-30 02:38:30 -0700

    working home page only

---
+++ frontend/scripts/script.js
@@ -0,0 +1,22 @@
+const arrowBtns = document.querySelectorAll(".more-button");
+const showcaseLists = document.querySelectorAll(".showcase-list");
+
+arrowBtns.forEach((arrow, i)=>{
+    const length = showcaseLists[i].querySelectorAll("img").length;
+    let count = 0;
+    arrow.addEventListener("click", ()=>{
+        count++;
+        if(length - (4 + count) > 0){
+            showcaseLists[i].style.transform = `translateX(${
+                showcaseLists[i].computedStyleMap().get("transform")[0].x.value -200
+            }px)`;
+ 
+        }
+        else{
+            showcaseLists[i].style.transform = "translateX(0)";
+            count = 0;
+        }
+        //May want to fix margin on scroll right doesnt matter rn
+    })
+
+});
