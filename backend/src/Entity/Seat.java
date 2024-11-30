595d4ba3c767bc8a35b706d105905dc1539f7cbf
jaybarc <jerome.barcelona@gmail.com>
2024-11-30 14:41:14 -0700

    seat classes

--- backend/src/Entity/Seat.java
+++ backend/src/Entity/Seat.java
@@ -1,4 +1,39 @@
 package Entity;
 
 public class Seat {
+    private int rowNum;
+    private int seatNumber;
+    private String status;
+
+    // Constructor
+    public Seat(int rowNum, int seatNumber, String status) {
+        this.rowNum = rowNum;
+        this.seatNumber = seatNumber;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public int getRowNum() {
+        return rowNum;
+    }
+
+    public void setRowNum(int rowNum) {
+        this.rowNum = rowNum;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
