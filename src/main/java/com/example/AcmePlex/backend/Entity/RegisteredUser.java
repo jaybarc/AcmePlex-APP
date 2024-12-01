c5e39e0a7e9b1105c787a5fc3bc12c33d154df46
DavidNg32 <dangducduy.nguyen@ucalgary.ca>
2024-12-01 14:02:41 -0700

    added function to pay annual fee for RegisteredUser

--- src/main/java/com/example/AcmePlex/backend/Entity/RegisteredUser.java
+++ src/main/java/com/example/AcmePlex/backend/Entity/RegisteredUser.java
@@ -1,9 +1,10 @@
 package com.example.AcmePlex.backend.Entity;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class RegisteredUser extends User{
+public class RegisteredUser extends User {
     private String username;
     private String password;
     private String firstName;
@@ -11,12 +12,11 @@ public class RegisteredUser extends User{
     private String address;
     private UserBankingInfo paymentInfo;
     private Date dateToPayFee;
-
     private List<Announcement> receivedAnnouncements;
-    
-    public RegisteredUser(int userID, boolean userType, String userEmail, String username, 
-                          String password, String firstName, String lastName, String address, UserBankingInfo paymentInfo
-            , Date dateToPayFee) {
+
+    public RegisteredUser(int userID, boolean userType, String userEmail, String username,
+                          String password, String firstName, String lastName, String address, UserBankingInfo paymentInfo,
+                          Date dateToPayFee) {
         super(userID, userType, userEmail);
         this.username = username;
         this.password = password;
@@ -27,11 +27,6 @@ public class RegisteredUser extends User{
         this.dateToPayFee = dateToPayFee;
     }
 
-    /**
-     * Receives an announcement and stores it in the user's list of received announcements.
-     *
-     * @param announcement The announcement to receive.
-     */
     public void receiveAnnouncement(Announcement announcement) {
         if (announcement == null) {
             throw new IllegalArgumentException("Announcement cannot be null.");
@@ -39,48 +34,74 @@ public class RegisteredUser extends User{
         receivedAnnouncements.add(announcement);
         System.out.println("Announcement received");
     }
-    
+
+    public void payAnnualFee() {
+        // Simulate payment process
+        System.out.println("Processing annual fee payment for user: " + username);
+
+        // Update the dateToPayFee to the next year
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateToPayFee);
+        calendar.add(Calendar.YEAR, 1);
+        dateToPayFee = calendar.getTime();
+
+        System.out.println("Annual fee paid. Next payment due date: " + dateToPayFee);
+    }
+
+    // Getters and setters
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public UserBankingInfo getPaymentInfo() {
         return paymentInfo;
     }
+
     public void setPaymentInfo(UserBankingInfo paymentInfo) {
         this.paymentInfo = paymentInfo;
     }
+
     public Date getDateToPayFee() {
         return dateToPayFee;
     }
+
     public void setDateToPayFee(Date dateToPayFee) {
         this.dateToPayFee = dateToPayFee;
     }
-    
 }
