88c790a916a448dd6038d23468c8befe17dac6a1
jaivalp22 <jaival.patel@ucalgary.ca>
2024-12-01 19:56:40 -0700

    j

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -32,3 +32,28 @@ arrowBtns.forEach((arrow, i)=>{
     })
 
 });
+
+document.getElementById("search-icon").addEventListener("click", function(){
+    const searchInput = document.getElementById("search-input");
+    if(searchInput.style.display === "none" || searchInput.style.display === ""){
+    searchInput.style.display = "block";
+    searchInput.focus();
+}
+    else{
+        searchInput.style.display = "none";
+    }
+});
+
+document.getElementById('search-bar').addEventListener('input', function () {
+    const query = this.value.toLowerCase().trim(); // Get the search query
+    const movieItems = document.querySelectorAll('.showcase-item'); // Get all movie items
+
+    movieItems.forEach(item => {
+        const movieName = item.getAttribute('data-name').toLowerCase(); // Get the movie name
+        if (movieName.includes(query)) {
+            item.classList.remove('hidden'); // Show if it matches the query
+        } else {
+            item.classList.add('hidden'); // Hide if it doesn't match
+        }
+    });
+});
